5148ce8cf54b54bdf6f722d3366d20d452f5de1f
vardlloo <vzardlloo.gmail.com>
2017-11-10 22:51:58 +0800

    project init

---
+++ src/main/java/com/vzard/educms/EduCmsApplication.java
@@ -0,0 +1,12 @@
+package com.vzard.educms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduCmsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vzard/educms/EduCmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vzard.educms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EduCmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
