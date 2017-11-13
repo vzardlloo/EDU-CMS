fd4f0cf0574baddafa9f91184f59547a6eaaf6f3
vardlloo <vzardlloo.gmail.com>
2017-11-13 13:33:41 +0800

    add course CRUD

---
+++ src/main/java/com/vzard/educms/mapper/CourseMapper.java
@@ -0,0 +1,40 @@
+package com.vzard.educms.mapper;
+
+import com.vzard.educms.database.tables.interfaces.ICourse;
+import com.vzard.educms.database.tables.pojos.Course;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vzard
+ * Date: 2017/11/13
+ * Time: 13:27
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ **/
+
+public class CourseMapper {
+
+    public static Course mapToDao(ICourse iCourse){
+
+        if (null == iCourse){
+            return null;
+        }
+        Course course = new Course();
+
+        course.setNumber(iCourse.getNumber());
+        course.setName(iCourse.getName());
+        course.setCredit(iCourse.getCredit());
+        course.setPeried(iCourse.getPeried());
+        course.setClassroom(iCourse.getClassroom());
+        course.setCourseTime(iCourse.getCourseTime());
+        course.setMaxChoosed(iCourse.getMaxChoosed());
+        course.setHasChoosed(iCourse.getHasChoosed());
+        course.setGrade(iCourse.getGrade());
+        course.setPacificGrade(iCourse.getPacificGrade());
+        course.setGeneralGrade(iCourse.getGeneralGrade());
+
+        return course;
+
+    }
+
+}
---
+++ src/main/java/com/vzard/educms/repository/CourseRepository.java
@@ -0,0 +1,117 @@
+package com.vzard.educms.repository;
+
+import com.vzard.educms.constant.ErrorConstant;
+import com.vzard.educms.database.Tables;
+
+
+import com.vzard.educms.database.tables.interfaces.ICourse;
+import com.vzard.educms.database.tables.interfaces.ITeacher;
+
+import com.vzard.educms.database.tables.pojos.Course;
+import com.vzard.educms.error.EduErrorException;
+import org.jooq.DSLContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vzard
+ * Date: 2017/11/13
+ * Time: 13:02
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ **/
+@Service
+public class CourseRepository {
+
+    @Autowired
+    @Qualifier("edu")
+    DSLContext dsl;
+
+
+    Logger logger = LoggerFactory.getLogger(CourseRepository.class);
+
+    public ICourse findCourseInfoByNumber(String number){
+
+        return dsl.select()
+                .from(Tables.COURSE)
+                .where(Tables.COURSE.NUMBER.eq(number)).fetchOneInto(ICourse.class);
+
+    }
+
+
+    public ICourse addCourseInfo(Course course){
+
+        if (isCourseExist(course.getNumber())){
+            throw new EduErrorException(ErrorConstant.ERROR_MSG_USER_EXIST_ERROR,ErrorConstant.ERROR_CODE_USER_NOT_FOUND);
+        }
+
+        dsl.insertInto(Tables.COURSE)
+                .columns(Tables.COURSE.NUMBER,Tables.COURSE.NAME,Tables.COURSE.CREDIT,
+                        Tables.COURSE.PERIED,Tables.COURSE.CLASSROOM,Tables.COURSE.COURSE_TIME,
+                        Tables.COURSE.MAX_CHOOSED,Tables.COURSE.HAS_CHOOSED,Tables.COURSE.GRADE,
+                        Tables.COURSE.PACIFIC_GRADE,Tables.COURSE.GENERAL_GRADE)
+                .values(course.getNumber(),course.getName(),course.getCredit(),
+                        course.getPeried(),course.getClassroom(),course.getCourseTime(),
+                        course.getMaxChoosed(),course.getHasChoosed(),course.getGrade(),
+                        course.getPacificGrade(),course.getGeneralGrade())
+                .execute();
+
+        return findCourseInfoByNumber(course.getNumber());
+
+    }
+
+
+    public ICourse updateCourseInfo(Course course){
+
+        if (!isCourseExist(course.getNumber())){
+            throw new EduErrorException(ErrorConstant.ERROR_MSG_USER_NOT_FOUND,ErrorConstant.ERROR_CODE_USER_NOT_FOUND);
+        }
+
+        dsl.update(Tables.COURSE)
+                .set(Tables.COURSE.NAME,course.getName())
+                .set(Tables.COURSE.CREDIT,course.getCredit())
+                .set(Tables.COURSE.PERIED,course.getPeried())
+                .set(Tables.COURSE.CLASSROOM,course.getClassroom())
+                .set(Tables.COURSE.COURSE_TIME,course.getCourseTime())
+                .set(Tables.COURSE.MAX_CHOOSED,course.getMaxChoosed())
+                .set(Tables.COURSE.HAS_CHOOSED,course.getMaxChoosed())
+                .set(Tables.COURSE.GRADE,course.getGrade())
+                .set(Tables.COURSE.PACIFIC_GRADE,course.getPacificGrade())
+                .set(Tables.COURSE.GENERAL_GRADE,course.getGeneralGrade())
+                .where(Tables.TEACHER.NUMBER.eq(course.getNumber()))
+                .execute();
+
+        return findCourseInfoByNumber(course.getNumber());
+
+    }
+
+
+
+    public void deleteCourseInfo(String number){
+
+        if (!isCourseExist(number)){
+            throw new EduErrorException(ErrorConstant.ERROR_MSG_USER_NOT_FOUND,ErrorConstant.ERROR_CODE_USER_NOT_FOUND);
+        }
+
+        dsl.delete(Tables.COURSE).where(Tables.COURSE.NUMBER.eq(number)).execute();
+
+    }
+
+
+
+
+
+    public Boolean isCourseExist(String number){
+        if (null != findCourseInfoByNumber(number)){
+            return true;
+        }
+
+        return false;
+    }
+
+
+}
